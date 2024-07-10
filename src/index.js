e056f855b5646253c87923cbc8e00a638f5840f2
Radhwana Kamal <r.kamal@ph.goit.global>
2024-07-10 18:00:09 +0800

    Debug refactor

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-//import { ImagesProvider } from 'context/ImagesProvider';
+import { ImagesProvider } from 'context/ImagesProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-     <App />
-    
+  <React.StrictMode>
+    <ImagesProvider>
+      <App />
+    </ImagesProvider>
   </React.StrictMode>
 );
